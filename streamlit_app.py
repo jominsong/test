9d907e3ced0825db5e3f6b3e7c1dca6efbb39857
jominsong <kai42tama1012@gmail.com>
2025-04-30 15:35:25 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
-color = st.color_picker("Pick A Color", "#23f513")
-st.write("The current color is", color)
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
