06bfc8d615c0e968168fbe071be93ff4a205b04a
jominsong <kai42tama1012@gmail.com>
2025-05-07 15:48:13 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+st.sidebar.page_link("pages/1_Home.py", label="홈")
+st.sidebar.page_link("pages/2_Chatbot.py", label="Chatbot")
+
 st.set_page_config(page_title="AI 멀티페이지 앱", layout="centered")
 st.title("🎉 AI 웹앱에 오신 걸 환영합니다!")
 st.markdown("👉 왼쪽 사이드바에서 **Chatbot**을 클릭해 대화를 시작해보세요.")
